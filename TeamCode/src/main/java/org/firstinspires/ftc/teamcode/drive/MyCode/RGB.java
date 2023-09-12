75dc90f9801e636a868d19d384a15bc0e0087b3e
wtinker <will.c.tinker@gmail.com>
2023-09-11 23:16:56 -0700

    establishing classes to hold basic methods we'll definetly be using this year

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/MyCode/RGB.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode.drive.MyCode;
+
+public class RGB {
+    public void SetBlue(){
+
+    }
+    public void SetRed(){
+
+    }
+    public void SetGreen(){
+
+    }
+    public void SetEndgame(int time){
+
+    }
+    public void Run(){
+
+    }
+}
