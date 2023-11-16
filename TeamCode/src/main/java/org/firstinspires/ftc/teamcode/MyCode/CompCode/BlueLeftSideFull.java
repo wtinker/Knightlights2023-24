80c35365dbc06803b0dacd224f512ce52368b3f6
wtinker <will.c.tinker@gmail.com>
2023-11-16 12:21:42 -0700

    coding in class goes brr

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyCode/CompCode/BlueLeftSideFull.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyCode/CompCode/BlueLeftSideFull.java
@@ -12,46 +12,46 @@ import org.firstinspires.ftc.teamcode.MyCode.Storage;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.trajectorysequence.TrajectorySequence;
 
-@Autonomous(name = "RedRightSideFull")
-public class RedRightSideFull extends LinearOpMode {
+@Autonomous(name = "BlueLeftSideFull")
+public class BlueLeftSideFull extends LinearOpMode {
     SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
     Output output = new Output(this, hardwareMap);
     Intake intake = new Intake(this, hardwareMap);
     boolean left = false;
     boolean middle = false;
     boolean right = true;
-    TrajectorySequence t1 = drive.trajectorySequenceBuilder(new Pose2d(12, -63, Math.toRadians(180)))
-            .lineTo(new Vector2d(12, -34))
+    TrajectorySequence t1 = drive.trajectorySequenceBuilder(new Pose2d(12, 63, Math.toRadians(180)))
+            .lineTo(new Vector2d(12, 34))
             .addDisplacementMarker(() ->{
                 //scan and score
             })
-            .turn(Math.toRadians(90))
+            .turn(Math.toRadians(-90))
             .addDisplacementMarker(() ->{
                 //scan and score
             })
-            .turn(Math.toRadians(90))
+            .turn(Math.toRadians(-90))
             .addDisplacementMarker(() ->{
                 //scan and score
             })
-            .splineToConstantHeading(new Vector2d(12, -48), Math.toRadians(0))
-            .splineTo(new Vector2d(51, -32), Math.toRadians(0))
+            .splineToConstantHeading(new Vector2d(12, 48), Math.toRadians(0))
+            .splineTo(new Vector2d(51, 42), Math.toRadians(0))
             .build();
     TrajectorySequence trajright1 = drive.trajectorySequenceBuilder(t1.end())
-            .strafeRight(8)
+            .strafeRight(10)
             .back(6)
-            .splineToLinearHeading(new Pose2d(36, -24, Math.toRadians(315)), Math.toRadians(0))
-            .lineTo(new Vector2d(48, -12))
+            .splineToLinearHeading(new Pose2d(36, 24, Math.toRadians(315)), Math.toRadians(0))
+            .lineTo(new Vector2d(48, 12))
             .build();
     Trajectory trajmid1 = drive.trajectoryBuilder(t1.end())
             .back(6)
-            .splineToLinearHeading(new Pose2d(36, -24, Math.toRadians(315)), Math.toRadians(0))
-            .lineTo(new Vector2d(48, -12))
+            .splineToLinearHeading(new Pose2d(36, 24, Math.toRadians(315)), Math.toRadians(0))
+            .lineTo(new Vector2d(48, 12))
             .build();
     TrajectorySequence trajleft1 = drive.trajectorySequenceBuilder(t1.end())
-            .strafeLeft(10)
+            .strafeLeft(8)
             .back(6)
-            .splineToLinearHeading(new Pose2d(36, -24, Math.toRadians(315)), Math.toRadians(0))
-            .lineTo(new Vector2d(48, -12))
+            .splineToLinearHeading(new Pose2d(36, 24, Math.toRadians(315)), Math.toRadians(0))
+            .lineTo(new Vector2d(48, 12))
             .build();
     public void runOpMode(){
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyCode/RGB.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MyCode/RGB.java
@@ -1,19 +1,47 @@
 package org.firstinspires.ftc.teamcode.MyCode;
 
-public class RGB {
-    public void SetBlue(){
+import com.qualcomm.hardware.rev.RevBlinkinLedDriver;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
 
+public class RGB {
+    private LinearOpMode myOpMode = null;
+    RevBlinkinLedDriver LED;
+
+    RevBlinkinLedDriver.BlinkinPattern red;
+    RevBlinkinLedDriver.BlinkinPattern blue;
+    RevBlinkinLedDriver.BlinkinPattern green;
+    RevBlinkinLedDriver.BlinkinPattern end;
+    RevBlinkinLedDriver.BlinkinPattern old;
+    RevBlinkinLedDriver.BlinkinPattern current;
+    public RGB(LinearOpMode opmode, HardwareMap hardwareMap){
+        myOpMode = opmode;
+        hardwareMap = hardwareMap;
+
+        LED = hardwareMap.get(RevBlinkinLedDriver.class, "LED");
+
+        red = RevBlinkinLedDriver.BlinkinPattern.RED;
+        blue = RevBlinkinLedDriver.BlinkinPattern.BLUE;
+        green = RevBlinkinLedDriver.BlinkinPattern.GREEN;
+        end = RevBlinkinLedDriver.BlinkinPattern.HEARTBEAT_WHITE;
+    }
+    boolean inEndGame = false;
+    boolean isRed;
+    public void SetBlue(){
+        isRed = false;
     }
     public void SetRed(){
-
+        isRed = true;
     }
     public void SetGreen(){
 
     }
-    public void SetEndgame(int time){
-
-    }
-    public void Run(){
-
+    public void Run(double time){
+        old = current;
+        if(time > 90){inEndGame = true;}
+        if(inEndGame){current = end;}
+        else if(isRed){current = red;}
+        else if(!isRed){current = blue;}
+        LED.setPattern(current);
     }
 }
